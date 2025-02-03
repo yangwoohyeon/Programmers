1e62f5df1b93759e76ae7b9e5dd0b99342882715
yangwoohyeon <dngus0717@gmail.com>
2025-02-03 03:56:42 -0800

    [level 3] Title: 섬 연결하기, Time: 1.01 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[][] costs) {
+        int answer = 0;
+        int[] island = new int[n];
+        
+        boolean[] visit = new boolean[n];
+        for(int i=0; i<n; i++){
+            island[i]=i;    
+        }
+        
+        Arrays.sort(costs,(a,b)->Integer.compare(a[2],b[2]));
+        
+        for(int i=0; i<costs.length; i++){
+            if(find(island, costs[i][0])!=find(island,costs[i][1])){
+                unite(island,costs[i][0],costs[i][1]);
+                answer+=costs[i][2];
+            }
+        }
+        return answer; 
+    }
+    
+    private int find(int[] island, int x){
+        if(island[x]==x){
+            return x;
+        }
+        return find(island,island[x]);
+    }
+    
+    private void unite(int[] island, int x, int y){
+        int a = find(island,x);
+        int b = find(island,y);
+        
+        island[a]=b;
+    }
+}
