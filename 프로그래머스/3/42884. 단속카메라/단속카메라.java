2524ff33c610baccf7432ca5d63de21e47398b65
yangwoohyeon <dngus0717@gmail.com>
2025-02-03 23:50:24 -0800

    [level 3] Title: 단속카메라, Time: 8.98 ms, Memory: 58.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] routes) {
+        int answer = 0;
+        boolean[] car = new boolean[routes.length];
+        
+          Arrays.sort(routes, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] route1, int[] route2) {
+                return route1[1] - route2[1];
+            }
+        });
+        
+        int cam = Integer.MIN_VALUE;
+        
+        for(int[] route : routes){
+            if(cam<route[0]){
+                cam=route[1];
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
