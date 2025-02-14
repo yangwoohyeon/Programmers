11fd75c9d393b10cae20d03464d8a1cb9dce3045
yangwoohyeon <dngus0717@gmail.com>
2025-02-14 04:59:05 -0800

    [level 2] Title: 멀리 뛰기, Time: 0.11 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static long answer = 0;
+    public long solution(int n) {
+        long[] dp = new long[n+2];
+        
+        dp[1]=1;
+        dp[2]=2;
+        if(n==1){
+            return 1;
+        }
+        if(n==2){
+            return 2;
+        }
+        for(int i=3; i<=n; i++){
+            dp[i]=(dp[i-1]+dp[i-2])%1234567;
+        }
+        return dp[n];
+    }
+}
