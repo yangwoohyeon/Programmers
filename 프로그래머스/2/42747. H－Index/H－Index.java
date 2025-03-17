6b7dece5c5f0d4b50f5ebd065b84cff655785230
yangwoohyeon <dngus0717@gmail.com>
2025-03-17 21:40:58 +0900

    [level 2] Title: H-Index, Time: 1.24 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,15 +2,16 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
-        int answer = 0;
         Arrays.sort(citations);
-       for(int i=0; i<citations.length; i++){
-           int h_index=citations.length-i;
-           if(citations[i]>=h_index){
-               answer=h_index;
-               break;
-           }
-       }
+        int answer = 0;
+        for(int i=0; i<citations.length; i++){
+            int h = citations[i];
+            if(h >= citations.length-i){
+                answer=citations.length-i;
+                break;
+            }
+        }
+        
         return answer;
     }
 }
