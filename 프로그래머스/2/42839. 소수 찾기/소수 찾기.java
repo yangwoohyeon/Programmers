46d8008cb50a9cf9aa0b8012782e6dc11671d16c
yangwoohyeon <dngus0717@gmail.com>
2025-03-19 12:49:46 +0900

    [level 2] Title: 소수 찾기, Time: 35.64 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -2,22 +2,21 @@ import java.util.*;
 
 class Solution {
     static Set<Integer> set;
-    static boolean[] visit = new boolean[7];
-    
+    static boolean[] visit;
     public int solution(String numbers) {
-        int answer = 0;
         set = new HashSet<>();
-        int size = numbers.length();
+        visit = new boolean[numbers.length()];
         
         dfs(numbers,"",0);
-        
-        for(int num : set){
-            if(check(num)){
+        int answer = 0;
+        for(int n : set){
+            if(check(n)){
                 answer++;
             }
-        }        
+        }
         return answer;
     }
+    
     public static void dfs(String numbers, String s, int depth){
         if(depth>numbers.length()){
             return;
@@ -25,21 +24,25 @@ class Solution {
         for(int i=0; i<numbers.length(); i++){
             if(!visit[i]){
                 visit[i]=true;
-                set.add(Integer.parseInt(s+numbers.charAt(i)));
-                dfs(numbers,s+numbers.charAt(i),depth+1);
+                String a = s+numbers.charAt(i);
+                set.add(Integer.parseInt(a));
+                dfs(numbers,a,depth+1);
                 visit[i]=false;
             }
+            
         }
     }
-    public static boolean check(int num){
-        if(num<2){
+    public static boolean check(int n){
+        if(n<2){
             return false;
         }
-        for(int i=2; i<num; i++){
-            if(num%i==0){
+        for(int i=2; i*i<=n; i++){
+            if(n%i==0){
                 return false;
             }
         }
+        
         return true;
     }
+    
 }
